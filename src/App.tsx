875725d026b40e05a2f104e6dba68ae9d571a94b
elanealencar <elanealencar@gmail.com>
2026-03-22 22:59:20 -0300

    commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+export default function App() {
+  return (
+    <div className="p-10">
+      <h1 className="text-red-800 text-4xl font-bold">
+        PAGE INICIAL - VITE + REACT + TAILWINDCSS - TESTE
+      </h1>
+    </div>
+  );
+}
